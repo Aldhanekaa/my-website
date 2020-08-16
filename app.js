9d4bfc7610b3b8596811943911b233f3f27510aa
Aldhanekaa <67791514+Aldhanekaa@users.noreply.github.com>
2020-08-16 14:16:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -22,9 +22,11 @@ modalClose.addEventListener('click', function () {
 let home = document.querySelector("#home");
 let about = document.querySelector("#about");
 let project = document.querySelector("#projects");
+let contact = document.querySelector("#contact")
 let home2 = document.querySelector('.welcome');
 let about2 = document.querySelector('.abouts');
 let project2 = document.querySelector('.project')
+let contact2 = document.querySelector('.contact')
 
 window.addEventListener('scroll', () => {
     var windo = window.pageYOffset;
@@ -33,17 +35,29 @@ window.addEventListener('scroll', () => {
         home2.classList.remove('wow')
         project2.classList.remove('wow')
 
-    } else if (project.offsetTop < windo) {
+    } else if (project.offsetTop <= windo && contact.offsetTop > windo) {
         about2.classList.remove('wow')
         home2.classList.remove('wow')
         project2.classList.add('wow')
-
+        contact2.classList.remove('wow')
+        console.log('project')
+    } else if (contact.offsetTop <= windo) {
+        about2.classList.remove('wow')
+        home2.classList.remove('wow')
+        project2.classList.remove('wow')
+        contact2.classList.add('wow')
+        console.log('contact')
     } else {
         about2.classList.remove('wow')
         home2.classList.add('wow')
+        contact2.classList.remove('wow')
         project2.classList.remove('wow')
     }
 })
+
+/** QUOTES */
+
+/** QUOTES */
 
 function peringatan() {
     alert('Max width is 1160px and max height is 730px')
